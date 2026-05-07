88a8b4aeb3b07ce33bf4b17b99a8bd97dd7d1d0a
Samuel Marques <samuel.s.marques@protonmail.com>
2026-05-07 12:21:14 -0300

    Create print_utils.py

---
+++ src/utils/print_utils.py
@@ -0,0 +1,18 @@
+from cmd2 import Color, stylize
+from rich.style import Style
+
+
+def success(message: str) -> None:
+    print(stylize(f"[+] {message}\n", Style(color=Color.GREEN)))
+
+
+def info(message: str) -> None:
+    print(stylize(f"[*] {message}\n", Style(color=Color.BLUE)))
+
+
+def error(message: str) -> None:
+    print(stylize(f"[-] {message}\n", Style(color=Color.RED)))
+
+
+def warn(message: str) -> None:
+    print(stylize(f"[!] {message}\n", Style(color=Color.YELLOW)))
