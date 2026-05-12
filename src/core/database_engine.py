d786da5db1ec13ca217006dd8e3505bbe1ac0569
Samuel Marques <samuel.s.marques@protonmail.com>
2026-05-12 11:24:04 -0300

    create dir if it doesn't exist

--- src/core/database_engine.py
+++ src/core/database_engine.py
@@ -1,8 +1,10 @@
+import os
 import sqlite3
 
 
 class DatabaseEngine:
     def __init__(self, db_path: str) -> None:
         self.path = db_path
+        os.makedirs(os.path.dirname(db_path), exist_ok=True)
         self.conn = sqlite3.connect(db_path, check_same_thread=False)
         self.conn.row_factory = sqlite3.Row
