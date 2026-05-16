6ab1cbd3c6d7ab6c4443a349f842d1b3c3f39876
Samuel Marques <samuel.s.marques@protonmail.com>
2026-05-16 01:16:03 -0300

    Update print_utils.py

--- src/utils/print_utils.py
+++ src/utils/print_utils.py
@@ -1,13 +1,17 @@
 from loguru import logger
 
+
 def success(message: str) -> None:
     logger.opt(depth=1).success(message)
+
 
 def info(message: str) -> None:
     logger.opt(depth=1).info(message)
 
+
 def error(message: str) -> None:
-    logger.opt(depth=1).error(message)
+    logger.opt(depth=2).error(message)
+
 
 def warn(message: str) -> None:
     logger.opt(depth=1).warning(message)
