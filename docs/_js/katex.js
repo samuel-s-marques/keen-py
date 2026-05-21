cb60c4877a0db81eaa18c06fc3551fe390470df9
Samuel Marques <samuel.s.marques@protonmail.com>
2026-05-21 14:28:26 -0300

    adapt to readthedocs

--- docs/_js/katex.js
+++ docs/_js/katex.js
@@ -1,10 +1,12 @@
-document$.subscribe(({ body }) => {
-    renderMathInElement(body, {
-        delimiters: [
-            { left: "$$", right: "$$", display: true },
-            { left: "$", right: "$", display: false },
-            { left: "\\(", right: "\\)", display: false },
-            { left: "\\[", right: "\\]", display: true }
-        ],
-    })
-})
+document.addEventListener("DOMContentLoaded", () => {
+    if (typeof renderMathInElement === "function") {
+        renderMathInElement(document.body, {
+            delimiters: [
+                { left: "$$", right: "$$", display: true },
+                { left: "$", right: "$", display: false },
+                { left: "\\(", right: "\\)", display: false },
+                { left: "\\[", right: "\\]", display: true }
+            ],
+        });
+    }
+});
